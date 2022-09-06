e580b60348f4c1cfe74d5bc22d9c8525c907ed4c
okabe <923048992@qq.com>
2022-09-06 09:24:52 +0800

    feat: allow cors

--- main.go
+++ main.go
@@ -35,7 +35,14 @@ type Illust struct {
 	urls    map[string]gjson.Result
 }
 
+func setupCORS(rw *http.ResponseWriter) {
+	(*rw).Header().Set("Access-Control-Allow-Origin", "*")
+	(*rw).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*rw).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func handlePixivProxy(rw http.ResponseWriter, req *http.Request) {
+	setupCORS(&rw)
 	var err error
 	var realUrl string
 	c := &Context{
